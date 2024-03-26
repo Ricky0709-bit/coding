142d3d130f33a4035b6f0ff7d6207eac521e9b04
Ricky <rpc7905@gmail.com>
2024-03-26 18:42:24 +0530

    Fixed

--- build/Debug/bank.cpp
+++ build/Debug/bank.cpp
@@ -76,44 +76,50 @@ int main()
   string business_name;
 
   
-  cout << "Enter Savings Account details:" << endl;
+  cout << "\nEnter Savings Account details:\n" << endl;
   cout << "Account Number: ";
   cin >> acc_no;
   cout << "Initial Balance: ";
   cin >> balance;
   cout << "Interest Rate (%): ";
   cin >> interest_rate;
+  cout<<"_____________________________________________________________\n"<<endl;
 
   Savingsacc saving(acc_no, balance, interest_rate / 100.0);
   saving.deposit(50000);
   saving.add_ir();
+  cout<<"Savings Account Details:\n"<<endl;
   saving.display();
 
-
-  cout << "\nEnter Checking Account details:" << endl;
+  cout<<"_____________________________________________________________\n"<<endl;
+  cout << "\nEnter Checking Account details:\n" << endl;
   cout << "Account Number: ";
   cin >> acc_no;
   cout << "Initial Balance: ";
   cin >> balance;
   cout << "Service Charge: ";
   cin >> service_charge;
+  cout<<"_____________________________________________________________\n"<<endl;
 
   Checkacc check(acc_no, balance, service_charge);
-  check.withdraw(500000);
+  check.withdraw(100000);
   check.servicecharge();
+  cout<<"Checking Account Details:\n"<<endl;
   check.display();
 
-  
-  cout << "\nEnter Business Account details:" << endl;
+  cout<<"_____________________________________________________________\n"<<endl;
+  cout << "Enter Business Account details:\n" << endl;
   cout << "Account Number: ";
   cin >> acc_no;
   cout << "Initial Balance: ";
   cin >> balance;
   cout << "Business Name: ";
   cin>> business_name;
+  cout<<"_____________________________________________________________\n"<<endl;
 
   Business busi(acc_no, balance, business_name);
-  busi.deposit(100000);
+  busi.deposit(500000);
+  cout<<"Business Account Details:\n"<<endl;
   busi.display();
 
   return 0;
